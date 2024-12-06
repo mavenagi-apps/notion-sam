c2a439f4b9beed954dc14c6903e6b779b3477d08
Igor <91226859+igorien2k@users.noreply.github.com>
2024-12-06 10:34:25 -0500

    tests, wip

--- src/___tests___/notion.test.ts
+++ src/___tests___/notion.test.ts
@@ -1,1 +1,34 @@
-// TODO(igor): WIP
+import { describe, expect, it, beforeEach, afterAll, vi } from 'vitest';
+import { Client } from '@notionhq/client';
+import {fetchNotionPages} from "../utils/notion";
+
+
+
+describe('import from notion', () => {
+    let notion: Client = new Client({ auth: 'foo' });
+
+    beforeEach(() => {
+        vi.mock('@notionhq/client', () => {
+            return {
+                Client: vi.fn().mockImplementation(() => {
+                    return {
+                        search: vi.fn().mockResolvedValueOnce({
+                            has_more: true,
+                            next_cursor: 'next-cursor',
+                            results: [],
+                        }).mockResolvedValueOnce({
+                            has_more: false,
+                            next_cursor: null,
+                            results: [],
+                        }),
+                    };
+                }),
+            };
+        });
+    });
+
+    it('fetches notion pages', async () => {
+        const pages = await fetchNotionPages(notion);
+        expect(pages).toEqual([]);
+    });
+});
