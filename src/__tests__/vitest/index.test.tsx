4798935526157a7dac27c447f36c67b2684a8652
Leonardo Oteca <leonardo@mavenagi.com>
2024-12-19 16:20:37 -0500

    refactor: update to app template latest vertion

---
+++ src/__tests__/vitest/index.test.tsx
@@ -0,0 +1,33 @@
+import { SetupServerApi, setupServer } from 'msw/node';
+import { afterAll, afterEach, beforeAll, describe, expect, it, vi } from 'vitest';
+
+describe('Maven hooks', async () => {
+  let server: SetupServerApi;
+
+  beforeAll(async () => {
+    console.log('beforeAll');
+    server = setupServer();
+    server.listen();
+  });
+
+  afterEach(() => {
+    server.resetHandlers();
+  });
+
+  afterAll(() => {
+    server.close();
+    vi.restoreAllMocks();
+  });
+
+  describe('preInstall', () => {
+    it('should run without error', () => {
+      expect(1).toBe(1);
+    });
+  });
+
+  describe('postInstall', () => {
+    it('should run without error', () => {
+      expect(1).toBe(1);
+    });
+  });
+});
