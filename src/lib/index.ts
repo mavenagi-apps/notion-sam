efba96bf98fcbfaed9f347dc756f222633a2de63
Leonardo Oteca <leonardo@mavenagi.com>
2025-01-08 14:33:10 -0500

    refactor: remove unused depeendencies

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,2 @@
 export { SAMPLE_ACTION_ID, installSampleAction, runSampleAction } from './actions';
 export { upsertSampleKnowledge, refreshSampleKnowledge } from './knowledge';
-export { SAMPLE_USERS } from './users';
-export { handleSampleTrigger } from './triggers';
